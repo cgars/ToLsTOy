d6e28cddb9d6c163ae424630e67b969649e18756
cgars <christian@stuebeweg50.de>
2012-05-09 10:53:19 +0200

    Added stop button, therefore threading Added color choice instead of quarters really switch of Chip SI on value 0

--- ToLsTOy/gui.py
+++ ToLsTOy/gui.py
@@ -1,10 +1,13 @@
 from Tkinter import *
 import re
 import tkMessageBox
+import tkFileDialog
 import my_control
+import copy
 
 class MainFrame(Text):    
-    def __init__(self, parent, control = my_control.Control()):
+    def __init__(self, parent, control = my_control.Control):
+        control = control(self)
         scrollbar = Scrollbar(parent)
         Text.__init__(self, parent,bg='grey',yscrollcommand=scrollbar.set)
         parent.title('ToLsTOy - Twenty Led Tower cOntrol')
@@ -33,10 +36,29 @@ class MainFrame(Text):
         '''
         Method called by hitting the start Button
         '''
+        
         color_protocol = self.color_table.get_all_values()
         shock_protocol = self.shock_table.get_all_values()
+        
         if color_protocol and shock_protocol:
-            self.control.start(color_protocol, shock_protocol)
+            print 'hello'
+            self.config(bg='red')
+            self.button_ico_frame.config(bg='red')
+            self.start_all_button = Button(self.button_ico_frame, text="Stop", 
+                                       command=self.stop)
+            self.start_all_button.grid(row=0,column=1, sticky=E)
+            self.update()
+            self.control.set_values(color_protocol, shock_protocol)
+    
+    def stop(self):
+        self.control.stop = True
+
+    def thread_done(self):
+        self.start_all_button = Button(self.button_ico_frame, text="Start", 
+                                       command=self.start_all)
+        self.start_all_button.grid(row=0,column=1, sticky=E)
+        self.config(bg='grey')
+        self.button_ico_frame.config(bg='grey')
 
 class IdleFrame(Frame):
     '''
@@ -46,32 +68,33 @@ class IdleFrame(Frame):
         Frame.__init__(self, parent, relief=RIDGE, borderwidth=3)
         self.parent = parent
         Label(self, text="Idle colors for all", fg="black").grid(row=0)
-        Label(self, text="left color:", fg="black",relief=SUNKEN, 
+        Label(self, text="blue:", fg="black",relief=SUNKEN, 
                      borderwidth=1, justify=RIGHT).grid(row=1, column=0, 
                                                         sticky=E)
-        self.left_color = Text(self,height=1,width=7)
-        self.left_color.insert(END, '000,000')
-        self.left_color.grid(row=1, column=1)
-        Label(self,text="syntax: green,blue", fg="black").grid(row=1, column=2)
-        Label(self, text="right color:", fg="black",relief=SUNKEN, 
+        self.blue = Text(self,height=1,width=15)
+        self.blue.insert(END, '000,000,000,000')
+        self.blue.grid(row=1, column=1)
+        Label(self,text="syntax: A,B,C,D", fg="black").grid(row=1, column=2)
+        Label(self, text="green:", fg="black",relief=SUNKEN, 
                      borderwidth=1, justify=RIGHT).grid(row=2, column=0, 
                                                         sticky=E)
-        self.right_color = Text(self,height=1,width=7)
-        self.right_color.insert(END, '000,000')
-        self.right_color.grid(row=2, column=1)
-        self.left_color.bind('<KeyRelease>',self.key_pressed)
+        self.green = Text(self,height=1,width=15)
+        self.green.insert(END, '000,000,000,000')
+        self.green.grid(row=2, column=1)
+        self.blue.bind('<KeyRelease>',self.key_pressed)
+        self.green.bind('<KeyRelease>',self.key_pressed)
     
     def get_colors(self):
         '''
         returns the colors set by the user in this frame
-        returns: ([left_green,left_blue],[right_green,right_blue])
+        returns: ([blue_A,blue_B,blue_C,blue_D],[green_A,green_B,green_C,green_D])
         '''
         # Error checking needed
-        pattern ='\d+,\d+$'
-        if re.match(pattern, self.left_color.get(1.0, END)):
+        pattern ='\d+,\d+,\d+,\d+$'
+        if re.match(pattern, self.blue.get(1.0, END)) and re.match(pattern, self.green.get(1.0, END)):
             self.config(bg='green')
-            return ([int(e) for e in self.left_color.get(1.0, END)[:7].split(',')],
-               [int(e) for e in self.right_color.get(1.0, END)[:7].split(',')])
+            return ([int(e) for e in self.blue.get(1.0, END)[:15].split(',')],
+               [int(e) for e in self.green.get(1.0, END)[:15].split(',')])
         else:
             self.config(bg='red')
             return 0
@@ -115,12 +138,75 @@ class  ProtocolTable(Frame):
         else:
             result
     
-    def load(self):
-        pass
     def save(self):
+        _file = tkFileDialog.asksaveasfile(mode='w')
         data = self.get_all_values()
         if data:
-            print data
+            for e in data[:-1]:
+                for i in e:
+                    _file.write('%s\t'%i)
+                _file.write('\n')
+    
+    def load(self):
+        _file = tkFileDialog.askopenfile(mode='r')
+        txt = _file.readlines()
+        txt.reverse()
+        self.rows=[]
+        for counter,e in enumerate(txt):
+            this_row = [eval(i.strip('\n')) for i in e.split('\t')[:-1]]
+            self.add_row(self.row_offset)
+            row = self.rows[0]
+            for text,this_text in zip(row.texts[1:],this_row):
+                if this_text:
+                    event = dummy()
+                    event.widget = text
+                    text.insert(1.0,str(this_text).strip(']').strip('[').replace(' ',''))
+                    self.text_field_change(event)
+    
+    def text_field_change(self, event):
+        row = int(event.widget.grid_info()['row'])
+        row = self.rows[row-self.row_offset]
+        column = int(event.widget.grid_info()['column'])
+        if column == 1:
+            if len(event.widget.get(1.0,END))>1:
+                [e.delete(1.0, END) for e in row.texts[2:]]
+                [e.config(bg='black',state=DISABLED) for e in row.texts[2:]]
+            else:
+                [e.config(bg='white', state=NORMAL) for e in row.texts[2:]]
+            
+            value = re.findall('\d+,\d+', event.widget.get(1.0,END))
+            if value:
+                event.widget.config(bg='green')
+                value = value[0].split(',')
+                row.values[column-1] = [int(value[0]),int(value[1])]
+            elif not len(event.widget.get(1.0,END))>1:
+                event.widget.config(bg='white')
+                row.values[column-1] = False
+            else:
+                event.widget.config(bg='red')
+                row.values[column-1] = False                
+
+        elif column == 2:
+            if len(event.widget.get(1.0,END))>1:
+                row.texts[1].delete(1.0, END)
+                row.texts[1].config(bg='black',state=DISABLED)
+            else:
+                row.texts[1].config(bg='white', state=NORMAL)
+            
+            value = re.findall('\d+$|\d+\.\d+', event.widget.get(1.0,END))
+            if value:
+                event.widget.config(bg='green')
+                value = float(value[0])
+                row.values[column-1] = value
+            
+            elif not len(event.widget.get(1.0,END))>1:
+                event.widget.config(bg='white')
+                row.values[column-1] = False
+            else:
+                event.widget.config(bg='red')
+                row.values[column-1] = False
+        
+        
 
 class LightTable(ProtocolTable):
     '''
@@ -128,71 +214,38 @@ class LightTable(ProtocolTable):
     '''
     def __init__(self, parent):
         ProtocolTable.__init__(self, parent,
-                               ['line','jmp,#','time[s]','G_l,B_l','G_r,B_r'],
-                               [8,8,8,8,8])    
+                               ['line','jmp,#','time[s]','Blue','Green'],
+                               [8,8,8,15,15])    
     def get_all_values(self):
         '''
         Evaluate the Lines of the Light Table
         Show a Dialog when an mismatch happend and return False
         Return the well formatted lines otherwise        
         '''
-        result = []
-        for line in [e.get_all_values() for e in self.rows]:
-            _tmp_result = [False,False,False,False]
-            # check jmp line
-            if  len(line[1])>1:
-                pattern = 'd+,\d+$'
-                value = re.findall(pattern, line[1])
-                if value:
-                    value = value[0]
-                    value = value.split(',')
-                    _tmp_result[0]=[int(value[0]),int(value[1])]
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column jmp;#'%line[0])
-                    return False
-            # check time line
-            if  len(line[2])>1:
-                pattern = '\d+$|\d+\.\d+'
-                value = re.findall(pattern, line[2])
-                if value:
-                    value = value[0]
-                    value = float(value)
-                    _tmp_result[1]=int(value)
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column time[s];#'%line[0]) 
-                    return False  
-            
-            # check left led line
-            if  len(line[3])>1:
-                pattern = '\d+,\d+'
-                value = re.findall(pattern, line[3])
-                if value:
-                    value = value[0]
-                    value = value.split(',')
-                    _tmp_result[2]=[int(value[0]),int(value[1])]
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column G_l,B_l;#'%line[0])
-                    return False
-            
-            # check right led line
-            if  len(line[4])>1:
-                pattern = '\d+,\d+'
-                value = re.findall(pattern, line[4])
-                if value:
-                    value = value[0]
-                    value = value.split(',')
-                    _tmp_result[3]=[int(value[0]),int(value[1])]
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column G_r,B_r;#'%line[0])
-                    return False
-            result.append(_tmp_result)
-        result.append([False,1,self.parent.idle_frame.get_colors()[0],
-                       self.parent.idle_frame.get_colors()[1]])
+        result = [e.values for e in self.rows]
+        result = copy.deepcopy(result)
+        result.append([False,1,[0,0,0,0],[0,0,0,0]])
         return result
+    
+    def text_field_change(self, event):
+        ProtocolTable.text_field_change(self, event)
+        row = int(event.widget.grid_info()['row'])
+        row = self.rows[row-self.row_offset]
+        column = int(event.widget.grid_info()['column'])
+        if column >= 3 :
+            value = re.findall('\d+,\d+,\d+,\d+', event.widget.get(1.0,END))
+            if value:
+                event.widget.config(bg='green')
+                value = value[0].split(',')
+                row.values[column-1] = [int(e) for e in value]
+            elif not len(event.widget.get(1.0,END))>1:
+                event.widget.config(bg='white')
+                row.values[column-1] = False
+            else:
+                event.widget.config(bg='red')
+                row.values[column-1] = False             
+
+                
 
 class ShockTable(ProtocolTable):
     '''
@@ -200,70 +253,67 @@ class ShockTable(ProtocolTable):
     '''
     def __init__(self,parent):
         ProtocolTable.__init__(self, parent, 
-                               ['line','jmp,#','time[s]','Intensity'],[8,8,8,8])
+                               ['line','jmp,#','time[s]','Intensity'],[8,8,8,8])   
+    
     def get_all_values(self):
         '''
         Evaluate the Lines of the Light Table
         Show a Dialog when an mismactch happens and return False
         Return the well formatted lines otherwise        
         '''
-        result = []
-        for line in [e.get_all_values() for e in self.rows]:
-            _tmp_result = [False,False,False]
-            # check jmp line
-            if  len(line[1])>1:
-                pattern = '\d+,\d+$'
-                value = re.findall(pattern, line[1])
-                if value:
-                    value = value[0]
-                    value = value.split(',')
-                    _tmp_result[0]=[int(value[0]),int(value[1])]
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column jmp,#'%line[0])
-                    return False
-            # check time line
-            if  len(line[2])>1:
-                pattern = '\d+$|\d+\.\d+'
-                value = re.findall(pattern, line[2])
-                if value:
-                    value = value[0]
-                    value = float(value)
-                    _tmp_result[1]=int(value)
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column time[s];#'%line[0]) 
-                    return False  
-            # check shock line
-            if  len(line[3])>1:
-                pattern = '\d+$'
-                value = re.findall(pattern, line[3])
-                if value:
-                    value = value[0]
-                    _tmp_result[2] = int(value)
-                else:
-                    tkMessageBox.showerror(
-                    'Uh Oh', 'Error in Line %s with column Intensity;#'%line[0])
-                    return False
-            result.append(_tmp_result)
+        result = [e.values for e in self.rows]
         result.append([False,1,0])
         return result
     
+    def text_field_change(self, event):
+        ProtocolTable.text_field_change(self, event)
+        row = int(event.widget.grid_info()['row'])
+        row = self.rows[row-self.row_offset]
+        column = int(event.widget.grid_info()['column'])
+        if column >= 3 :
+            value = re.findall('\d+', event.widget.get(1.0,END))
+            if value:
+                event.widget.config(bg='green')
+                row.values[column-1] = int(value[0])
+            elif not len(event.widget.get(1.0,END))>1:
+                event.widget.config(bg='white')
+                row.values[column-1] = False
+            else:
+                event.widget.config(bg='red')
+                row.values[column-1] = False   
+    
 class TableRow:
-    def __init__(self, parent,row, cols =[3,7,7,7,7] ):
+    def __init__(self, parent, row, cols =[3,7,7,7,7] ):
         self.parent = parent
         self.texts = []
         self.row = row
         for counter,col in enumerate(cols):
             _tmp = Text(parent,height=1,width=col,borderwidth=0)
             _tmp.grid(row=row, column = counter)
+            _tmp.bind('<KeyRelease>', parent.text_field_change)
             self.texts.append(_tmp)
         self.texts[0].insert(END, row)
         self.texts[0].config(state=DISABLED)
+        #self.texts[1].bind('<KeyRelease>', self.jumper_line_changed)
+        #self.texts[2].bind('<KeyRelease>', self.time_line_changed)
         self.add_line = Button(parent,text='add',command=self.add_row, height=1)
-        self.add_line.grid(row=row,column=5)
-        
-
+        self.add_line.grid(row=row,column=5)        
+        self.values = [False for e in cols]
+    
+#    def jumper_line_changed(self, event):
+#        if len(self.texts[1].get(1.0,END))>1:
+#            [e.delete(1.0, END) for e in self.texts[2:]]
+#            [e.config(bg='red',state=DISABLED) for e in self.texts[2:]]
+#        else:
+#            [e.config(bg='white', state=NORMAL) for e in self.texts[2:]]
+#    
+#    def time_line_changed(self, event):
+#        if len(self.texts[2].get(1.0,END))>1:
+#            self.texts[1].delete(1.0, END) 
+#            self.texts[1].config(bg='red',state=DISABLED)
+#        else:
+#            self.texts[1].config(bg='white', state=NORMAL)
+    
     def add_row(self):
         self.parent.add_row(self.row)
 
@@ -280,7 +330,9 @@ class TableRow:
         
     def get_all_values(self):
         return [e.get(1.0,END) for e in self.texts]
-    
+
+class dummy:
+    pass  
 if __name__ == '__main__':
     root = Tk()
     app = MainFrame(root)
