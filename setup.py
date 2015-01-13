7b979208783f64a4a48d80b675a2dbe9111adee5
cgars <christian@stuebeweg50.de>
2015-01-13 15:38:07 +0100

    Licensing

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2015 Christian Garbers.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Simplified BSD License
+# which accompanies this distribution
+# 
+# Contributors:
+#     Christian Garbers - initial API and implementation
+#-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
 from distutils.core import setup
